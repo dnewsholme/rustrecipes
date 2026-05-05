b239fb1f174fcd034f799cb8fe2f88c4b3c69969
Daryl Newsholme <dbizsley@googlemail.com>
2026-05-05 22:51:30 +0100

    ui tests 5 second timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,10 +7,16 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 5000,
+  expect: {
+    timeout: 5000
+  },
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
+    navigationTimeout: 5000,
+    actionTimeout: 5000,
   },
 
   projects: [
